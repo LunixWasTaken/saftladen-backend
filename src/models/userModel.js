f24e9ba696eef3ddfa3b7bc289b7cb9b19d2e05f
Lunix <lunixwastaken@protonmail.com>
2021-09-13 15:52:19 +0200

    DB Connect

---
+++ src/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: UUID,
+    default: uuidv4
+  },
+  username: {
+    type: String,
+    required: "Username is required"
+  },
+  password: {
+    type: String,
+    required: "Uhm... Account without password? Help?"
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now()
+  },
+  lastLoginDate: {
+    type: Date,
+    default: 0
+  }
+});
+
+module.exports = User = mongoose.model('Users', userSchema);
---
+++ src/util/db.js
@@ -0,0 +1,20 @@
+import Mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+      await Mongoose.connect(
+        process.env.mongoAuth,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        }
+      );
+  
+      console.log('MongoDB is Connected...');
+    } catch (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  }
+  
+export default connectDB;
