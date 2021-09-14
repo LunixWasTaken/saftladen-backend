45a23568d1c7fbc70f196b0ab797f122773a651b
Lunix <lunixwastaken@protonmail.com>
2021-09-14 11:43:02 +0200

    Resturcturing and Progress

--- src/api/index.js
+++ src/api/index.js
@@ -3,20 +3,24 @@ import bodyParser from 'body-parser';
 import multer from 'multer';
 
 import userRoutes from './user';
+import productRoutes from './product';
+import orderRoutes from './order';
 
 const upload = multer();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(upload.array());
 
-//Use the Router on the sub route /movies
+// Use the Router on the sub route /movies
 app.get('/', (req, res) => {
-  res.status(200).end()
-})
+  res.status(200).end();
+});
 
-app.use('/user', userRoutes)
+app.use('/user', userRoutes);
+app.use('/product', productRoutes);
+app.use('/order', orderRoutes);
 
 
 
