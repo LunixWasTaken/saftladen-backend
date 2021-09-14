5d7ce3af2af4b2b7b81086a13c115ffd9f7c64fa
Lunix <lunixwastaken@protonmail.com>
2021-09-14 12:56:26 +0200

    Cosmetics

--- src/api/product/put.js
+++ src/api/product/put.js
@@ -6,21 +6,21 @@ const router = express.Router();
 
 
 router.put('/', (req, res, next) => {
-  if(!req.body) res.sendStatus(400);
+  if (!req.body) res.sendStatus(400);
   const prod = {
     name: req.body.name,
     price: req.body.price,
     description: req.body.description,
     img: req.body.img,
-    available: req.body.available
-  }
+    available: req.body.available,
+  };
   products.insert(prod, (err, data) => {
-    if(err) {
+    if (err) {
       return res.sendStatus(500);
       console.error(err);
     }
     res.status(201).type('application/json').send(data);
-  })
-})
+  });
+});
 
-export const pPut = router ;
+export const pPut = router;
