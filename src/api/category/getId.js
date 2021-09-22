c73659abdc185fe06516a0ee6e540f8e4bfb486d
Kevin B <lunixwastaken@protonmail.com>
2021-09-22 14:05:17 +0200

    Cosmetics

--- src/api/category/getId.js
+++ src/api/category/getId.js
@@ -9,7 +9,7 @@ router.get('/:id', async (req, res) => {
   Category.findOne({_id: req.params.id}, (err, data) => {
     if (err) return res.status(400).json({success: false, message: err});
     if (!data) return res.sendStatus(404);
-    res.status(200).type('json').send(JSON.stringify(data));
+    res.status(200).type('json').json(data);
   });
 });
 
